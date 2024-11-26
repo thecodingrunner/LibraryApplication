2d208662a123b44bb83b363743e96ab20663c91a
thecodingrunner <finn.moffett@gmail.com>
2024-11-26 11:46:43 +0000

    Create book manager and book classes
    Add AddBook method to Book Manager

---
+++ LibraryApplication/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApplication
+{
+    public class Book
+    {
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public string Author { get; private set; }
+        public DateOnly PublicationDate { get; private set; }
+        public int Pages { get; private set; }
+        public int BookId { get; private set; }
+        static int NumberOfBooks = 0;
+
+        public Book(string title, string description, string author, DateOnly publicationDate, int pages)
+        {
+            Title = title;
+            Description = description;
+            Author = author;
+            PublicationDate = publicationDate;
+            Pages = pages;
+            NumberOfBooks++;
+            BookId = NumberOfBooks;
+        }
+    }
+}
---
+++ LibraryApplication/BookManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApplication
+{
+    public class BookManager
+    {
+        public List<Book> books = new List<Book>();
+
+
+        public void AddBook(string title, string description, string author, DateOnly publicationDate, int pages)
+        {
+            Book book = new Book(title, description, author, publicationDate, pages);
+
+            books.Add(book);
+        }
+    }
+}
